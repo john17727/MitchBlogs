5145197a57578fe9bb413c207489d608af31de1a
john17727 <justanotherone17727@gmail.com>
2020-06-09 12:09:11 -0600

    Added forgot password webview.

--- app/src/main/java/com/codingwithmitch/openapi/util/Constants.kt
+++ app/src/main/java/com/codingwithmitch/openapi/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.codingwithmitch.openapi.util
 
 const val BASE_URL = "https://open-api.xyz/api/"
+const val PASSWORD_RESET_URL = "https://open-api.xyz/password_reset/"
 
 const val NETWORK_TIMEOUT = 3000L
 const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
